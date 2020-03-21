55f973e0dd8bf227cf7bd019a3de3da649efba28
Mirha Masala <mirha.masala@me.com>
2020-03-21 13:56:27 +0800

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "tours#show"
+
+  resources :tours, only: [:show] do
+    resources :spots, only: [:index]
+  end
 end
